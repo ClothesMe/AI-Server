0e7e68b23edc7da08a01c65921050be22806ed6a
5nam <ohnam90@gmail.com>
2024-04-15 17:02:47 +0900

    [feat] : 옷 정보 듣기 기능 구현을 위한 파일 생성

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import socks
 from fastapi import FastAPI
+from fastapi import File, UploadFile
 
 app = FastAPI()
 
@@ -24,3 +25,18 @@ def socks_color(socks_image_path):
          return {"왼쪽 양말의 색상은 ": left_color, "오른쪽 양말의 색상은 ": right_color, "양말의 짝이 ": "맞습니다."}
     else:
         return {"왼쪽 양말의 색상은 ": left_color, "오른쪽 양말의 색상은 ": right_color, "양말의 짝이 ": "맞지 않습니다."}
+    
+@app.post("/clothes")
+async def read_clothes(file: UploadFile):
+    UPLOAD_DIR = "./photo"  # 이미지를 저장할 서버 경로
+
+    image = await file.read() # 이미지 파일 받아오는 부분
+    # filename = f"{str(uuid.uuid4())}.jpg"  # uuid로 유니크한 파일명으로 변경
+    # with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
+    #     fp.write(content)  # 서버 로컬 스토리지에 이미지 저장 (쓰기)
+
+    return {"isSuccess": True,
+            "code": "2000",
+            "message": "Ok",
+            "result": color + "의 " + pattern + "패턴의 " + clothes_type + " 입니다."
+    }
