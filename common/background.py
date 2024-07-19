3bf4851b57cc7bf50e7b6f55390e123fcb82d8f9
5nam <ohnam90@gmail.com>
2024-07-20 00:38:27 +0900

    [error] : TypeError: a bytes-like object is required, not 'JpegImageFile 오류 해결 rembg 라이브러리의 remove 함수가 PIL.Image 객체가 아닌 바이트 형태의 데이터를 필요로 하기 때문에 오류 발생. 바이트 형태 그대로 넘겨주니 오류 해결

--- common/background.py
+++ common/background.py
@@ -2,19 +2,15 @@ from rembg import remove
 import numpy as np
 from PIL import Image
 
-def remove_background(image: np.ndarray) -> np.ndarray:
+def remove_background(clothes_image):
     """
     배경 제거
-    :param image: 배경 제거할 이미지 (numpy 배열)
-    :return: 배경이 제거된 이미지 (numpy 배열, 투명 배경)
-    """
-    # numpy 배열을 PIL 이미지로 변환
-    image_pil = Image.fromarray(image)
-
-    # 배경 제거
-    removed_background_pil = remove(image_pil)
-
-    # PIL 이미지를 numpy 배열로 변환
-    removed_background_np = np.array(removed_background_pil)
+    :param cloth_image: 배경 제거할 이미지
+    :return: 배경이 제거된 이미지
+    """   
+    removed_background = remove(clothes_image)
+        
+    # numpy 배열로 변환
+    removed_background_np = np.array(removed_background)
 
     return removed_background_np
